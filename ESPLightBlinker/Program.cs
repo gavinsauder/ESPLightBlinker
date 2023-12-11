2d0cab358186e0b3ecc05e67f437966632cf5458
Gavin Sauder <gavin@landistechnologies.com>
2023-12-11 13:30:20 -0500

    inital code

--- ESPLightBlinker/Program.cs
+++ ESPLightBlinker/Program.cs
@@ -1,20 +1,37 @@
 using System;
 using System.Diagnostics;
 using System.Threading;
+using System.Device.Gpio;
 
 namespace ESPLightBlinker
 {
     public class Program
     {
+        private static GpioController s_GpioController;
+
         public static void Main()
         {
+            s_GpioController = new GpioController();
+
             Debug.WriteLine("Hello from nanoFramework!");
+            // Following this: https://docs.nanoframework.net/content/getting-started-guides/getting-started-managed.html
 
-            Thread.Sleep(Timeout.Infinite);
+            GpioPin led = s_GpioController.OpenPin(4, PinMode.Output);
+            led.Write(PinValue.Low);
 
-            // Browse our samples repository: https://github.com/nanoframework/samples
-            // Check our documentation online: https://docs.nanoframework.net/
-            // Join our lively Discord community: https://discord.gg/gCyBu8T
+            while (true)
+            {
+                led.Toggle();
+                Thread.Sleep(125);
+                led.Toggle();
+                Thread.Sleep(125);
+                led.Toggle();
+                Thread.Sleep(125);
+                led.Toggle();
+                Thread.Sleep(525);
+            }
+
+
         }
     }
 }
